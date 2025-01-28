a7ff830ffd0a9adfd40432fe4fc7775784806100
Matteriel-Git <matthew.alpert@gmail.com>
2025-01-28 01:52:15 -0800

    updates

---
+++ backend/itemsRoutes.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const database = require('./connect');
+
+let itemsRoutes = express.Router();
+
+// - Retrieve all
+itemsRoutes.route('/items').get(async (req, res) => {
+    let db = database.getDb();
+    let data = await db.collection('items').find({}).toArray();
+    if (data.length > 0) {
+        res.json(data);
+    } else {
+        throw new Error('Data was not found');
+    }
+});
+
+// - Retrieve one
+// - Create one
+// - Update one
+// - Delete one
